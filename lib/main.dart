e654532f64a4331b4401ce74a26e6469f28c1eb1
Ayush Sahu <ayush.cbs1@gmail.com>
2023-03-15 08:47:57 +0530

    curveTween (ease out)

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,16 @@ Route _createRoute() {
   return PageRouteBuilder(
     pageBuilder: (context, animation, secondaryAnimation) => const Page2(),
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      return child;
+      const begin = Offset(0.0, 1.0);
+      const end = Offset.zero;
+      var curve = Curves.ease;
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+      final offsetAnimation = animation.drive(tween);
+
+      return SlideTransition(
+        position: offsetAnimation,
+        child: child,
+      );
     },
   );
 }
